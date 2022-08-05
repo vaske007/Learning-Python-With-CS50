ad13fab7379d711e686e46c2c061754af487294a
Vasilije Mrmak <vmrmak36@gmail.com>
2022-08-05 01:02:58 +0100

    Inital commit (What's your name)

---
+++ hello.py
@@ -0,0 +1,4 @@
+#by writing 'python' and the name of the file (that you want to run), in the terminal, the code runs itself
+#by writing 'code' and the name of the file you want to create, you create a new file.
+name = input("What's your name? ") # The 'input' function let's the user input a string(a sequence of text). Name is a variable, which has the value of  what's on the other side of the equals sign.
+print("Hello,", name) # The print function prints onto the screen whatever is within the parenthesis, as long as it is recongizable to the computer
