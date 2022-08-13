ff2c3c059cdb1905baa0fadd35e151b84f5c6426
Vasilije Mrmak <vmrmak36@gmail.com>
2022-08-14 00:02:26 +0100

    A calculator capable of addition

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# 'int', which is short for integers, is a number all the way toward negative infinity, and all the way toward positive infinity. But there is no decimal point in an integer.
+# Python, just like in math, looks at the innermost parentheses first, and when it gets its answer, it continues to the next parentheses...
+
+x = int(input("What's x? ")) # Input function gets called first and then the result of that becomes the inputto the int function
+y = int(input("What's y? ")) # Input function gets called first and then the result of that becomes the inputto the int function
+
+print(x + y)
