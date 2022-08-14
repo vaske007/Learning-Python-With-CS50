42565f642dcfcf9052482bb2b9bde826204006d5
Vasilije Mrmak <vmrmak36@gmail.com>
2022-08-14 21:27:37 +0100

    Saying hello to the user by making my own function with 'def'

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 # by writing 'python' and the name of the file (that you want to run), in the terminal, the code runs itself
 # by writing 'code' and the name of the file you want to create, you create a new file.
-# The 'input' function let's the user input a string(a sequence of text). Name is a variable, which has the value of  what's on the other side of the equals sign. Strip and title are defined further down.
-name = input("What's your name? ").strip().title()
-
-# Split user's name into first name and last name
-# Splits the string 'name' into multiple substrings, 'first' and 'last'
-first, last = name.split(" ")
+# by writing 'python' in your terminal, you enter the interactive mode. Where all you need to do, is write a line off code and press 'enter', and the code will run. To exit the interactive mode press Ctrl-z (on windows).
+# by writing 'def', which stands for define, you can create your own function.
 
 
-# Third way
-print(f"Hello, {first}")
+def hello(to): # We have created a function, called 'hello'. The inside of the parenthese is a parameter
+    print("Hello,", to)
+
+
+name = input("What's your name? ")
+hello(name) # Here we're using the value of the parameter, whcih is called 'to', to plug into print, so that we also see the persons name. What is happening here is even though the variable is called 'Name', when the function itself is called, the computer assumes that same value is now called to. 
