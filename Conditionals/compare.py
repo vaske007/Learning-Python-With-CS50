0dd501eb190c74127de48bbb66fbfc8cbe0ae9de
Vasilije Mrmak <vmrmak36@gmail.com>
2022-08-19 05:15:52 +0100

    Intial commit

---
+++ Conditionals/compare.py
@@ -0,0 +1,9 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y: # Here we asked a question is x less then y, if so print the code below. 'x < y' is called a Boolean expression, named after a mathemathician Bool. A Boolean expresion is a questions with a yes or no answer, or actually a true or false answer.
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
