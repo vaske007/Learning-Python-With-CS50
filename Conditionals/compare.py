a331313b0c3309c59056646708e56baad18acff4
Vasilije Mrmak <vmrmak36@gmail.com>
2022-08-20 21:53:08 +0100

    The or statement

--- Conditionals/compare.py
+++ Conditionals/compare.py
@@ -1,9 +1,8 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-if x < y: # Here we asked a question is x less then y, if so print the code below. 'x < y' is called a Boolean expression, named after a mathemathician Bool. A Boolean expresion is a questions with a yes or no answer, or actually a true or false answer.
-    print("x is less than y")
-elif x > y: # If the if statment above isn't true, the code will check whether or not this elif program is true
-    print("x is greater than y")
-else: # If none of the statements above are true, the code will run this line. The upside to using else, instead of elif, is faster code, which isn't noticeable now, but if we had a much bigger and more complex code, this would help it to run faster.
-    print("x is equal to")
+# If the first part of the if function is not true it will check the other part, that comes after or. If neither are true, it will print the else statement
+if x < y or x > y: 
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
