dee39da8d312da0b09164afc58f713cf55dd607b
Vasilije Mrmak <vmrmak36@gmail.com>
2022-08-29 19:35:14 +0100

    Simplifing or code to get what we want.

--- Conditionals/compare.py
+++ Conditionals/compare.py
@@ -1,8 +1,7 @@
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
-# If the first part of the if function is not true it will check the other part, that comes after or. If neither are true, it will print the else statement
-if x < y or x > y: 
+if x != y:
     print("x is not equal to y")
 else:
     print("x is equal to y")
